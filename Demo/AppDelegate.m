56e5b6ee326ea7f3c81de67fcf3c3cd5c7f9a9bb
Balazs Faludi <balazsfaludi@gmail.com>
2013-04-15 10:15:42 +0200

    Initial Commit

---
+++ Demo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Demo
+//
+//  Created by Balázs Faludi on 15.04.13.
+//  Copyright (c) 2013 Balazs Faludi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ Demo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Balázs Faludi on 15.04.13.
+//  Copyright (c) 2013 Balazs Faludi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
